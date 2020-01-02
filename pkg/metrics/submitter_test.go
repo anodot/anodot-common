535440d5835a2b42c58b693d2690fbe5d3c3320c
Vova Nekhai <vova.nehai@anodot.com>
2020-01-02 13:47:36 +0200

    fix escape chars

--- pkg/metrics/submitter_test.go
+++ pkg/metrics/submitter_test.go
@@ -2,6 +2,8 @@ package metrics
 
 import (
 	"encoding/json"
+	"fmt"
+	"reflect"
 	"strconv"
 	"testing"
 	"time"
@@ -29,11 +31,76 @@ func TestSubmitter(t *testing.T) {
 
 		excpedtedJonsOutput := `[{"properties":{"source":"gotest","target_type":"gauge","what":"test2"},"timestamp":1540153181,"value":1,"tags":{}},{"properties":{"source":"gotest","target_type":"gauge","what":"test2"},"timestamp":1540153181,"value":1,"tags":{}}]`
 		actualJson := string(b)
-		if actualJson != excpedtedJonsOutput {
+
+		equal, err := equalJson(actualJson, excpedtedJonsOutput)
+		if err != nil {
+			t.Fatalf(err.Error())
+		}
+
+		if !equal {
 			t.Fatalf("expected metrics json: %v, \n got: %v", excpedtedJonsOutput, actualJson)
 		}
 	}}
 	mocksubmitter.SubmitMetrics(metrics)
+}
+
+func TestSpecialChars(t *testing.T) {
+	var testData = []struct {
+		in          string
+		out         string
+		description string
+	}{
+		{"host.port", "host_port", "'.' in name"},
+		{"server name", "server_name", "'space' in name"},
+		{"name ", "name", "'space' at the end"},
+		{"value>S", "value\u003eS", "'>' in name"},
+		{"value<X", "value\u003cX", "'<' in name"},
+		{"value-1", "value-1", "'-' in name"},
+		{"api/v1", "api/v1", "'/' in name"},
+		{"host:8080api/v1", "host:8080api/v1", "':' in name"},
+		{"a\\m", "a\\\\m", "'\\' in name"},
+	}
+
+	t1 := time.Date(2016, time.August, 15, 0, 0, 0, 0, time.UTC)
+
+	for _, v := range testData {
+
+		t.Run(v.description, func(t *testing.T) {
+			metric := Anodot20Metric{Properties: map[string]string{"what": v.in, "target_type": "gauge", v.in: "remote_write"}, Timestamp: AnodotTimestamp{t1}, Value: 1, Tags: map[string]string{"key": v.in, v.in: "value"}}
+
+			bytes, err := json.Marshal(&metric)
+			if err != nil {
+				t.Fatalf(err.Error())
+			}
+
+			expectedJson := fmt.Sprintf(`{"properties":{"%[1]s":"remote_write","target_type":"gauge","what":"%[1]s"},"tags":{"key":"%[1]s","%[1]s":"value"},"timestamp":1471219200,"value":1}`, v.out)
+			equal, err := equalJson(string(bytes), expectedJson)
+			if err != nil {
+				t.Fatalf(err.Error())
+			}
+
+			if !equal {
+				t.Fatal(fmt.Sprintf("wrong metrics20 json\n got: %v\n want: %v", string(bytes), expectedJson))
+			}
+		})
+	}
+}
+
+func equalJson(s1, s2 string) (bool, error) {
+	var o1 interface{}
+	var o2 interface{}
+
+	var err error
+	err = json.Unmarshal([]byte(s1), &o1)
+	if err != nil {
+		return false, fmt.Errorf("error mashalling string 1 :: %s", err.Error())
+	}
+	err = json.Unmarshal([]byte(s2), &o2)
+	if err != nil {
+		return false, fmt.Errorf("error mashalling string 2 :: %s", err.Error())
+	}
+
+	return reflect.DeepEqual(o1, o2), nil
 }
 
 type MockSubmitter struct {
